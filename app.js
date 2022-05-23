9bade732ac10b8ace26fe5db30b033be196fe491
Verity Gregory <verity.gregory@gmail.com>
2022-05-23 16:04:04 +0100

    functionally working, css needs work

--- app.js
+++ app.js
@@ -6,15 +6,22 @@ const answerArray = [
 	`Outlook pretty hazy, let me clean my glasses`,
 	`Sounds like an epic idea, let me stand back before you do it though`,
 	`Hmm, I don't think that is a wise course of action`,
+	`It's your money, why not`,
+	`The early bird gets the worm, but the second mouse gets the cheese`,
+	`I wouldn't do that if I were you. But I'm not, so go for it`,
+	`I'm not sure, ask me again later`,
 ];
 const inputQuestion = document.getElementById('q-input');
 const submitBtn = document.getElementById('submit');
 const displayAnswer = document.getElementById('answer');
 const inputAnswer = document.getElementById('input-question');
-function askQuestion() {
-	console.log(inputQuestion.value);
-	displayAnswer.innerText = `You asked: ${inputQuestion.value}`;
-	const randomNumber = Math.floor(Math.random() * answerArray.length);
+const trev = document.getElementById('trev');
 
-	console.log(answerArray[randomNumber]);
+function askQuestion() {
+	const randomNumber = Math.floor(Math.random() * answerArray.length);
+	const question = inputQuestion.value;
+	inputQuestion.value = '';
+	inputAnswer.innerText = `You asked: ${question}`;
+	displayAnswer.innerText = `Trev's wisdom: "${answerArray[randomNumber]}"`;
+	trev.classList.add('vanish');
 }
